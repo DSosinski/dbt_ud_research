d9e05435c481d77a5361c853d39d2f9451ca6b0f
dsosinski <dsosinski@gmail.com>
2023-03-26 16:11:20 +0100

    Added dbt execute as python script

---
+++ dbt_research/dbt_execute.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+import boto3
+import json
+from datetime import datetime
+
+#subprocess.call("dbt --log-format=json test")  
+
+if True:
+    run_results = open('target/run_results.json', "r")
+    run_results_data = json.loads(run_results.read())
+    run_results.close()
+
+    message = {"default": "default"}
+    client = boto3.client('sns')
+    arn = os.environ.get('AWS_SNS_TOPIC_ARN')
+    response = client.publish(
+        TargetArn=arn,
+        Message=json.dumps({'default': json.dumps(message),
+                            'email': json.dumps(run_results_data, indent=2)
+                            }),
+        Subject='DUR run on '+ datetime.today().isoformat(),
+        MessageStructure='json'
+    )
